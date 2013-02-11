13f2b2e883e78c00be1053361086db93e8e93287
Steven Foster <stevensfoster@gmail.com>
2013-02-11 11:56:27 -0800

    adding files
    palindromeValidate.pl file and shortPalindrome.txt file

---
+++ palindromeValidate.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+#
+# Basic script for reading in a file
+# storing a file as a string
+# reversing the string
+# and checking to see if the reverse
+# equals the original
+#
+
+use strict;
+use warnings;
+use File::Slurp;
+
+my $forward = read_file( 'shortPalindrome.txt' );
+my $reverse = reverse $forward;
+
+print $forward, "\n";
+print $reverse, "\n";
+
+
+if ($forward eq $reverse) { 
+	print "It's a palindrome!\n";
+} else {
+	print "It's not a palindrome!\n";
+}
